ebd5602e151a0e338b8d8d8a6b90c1e11a3f1560
Vedant-Chakkarwar <100403628+Vedant-Chakkarwar@users.noreply.github.com>
2024-09-25 14:34:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'src\index.css';
-import App from 'src\App.js';
+import './src/index.css';
+import App from './src/App.js';
 
 ReactDOM.render(
   <React.StrictMode>
