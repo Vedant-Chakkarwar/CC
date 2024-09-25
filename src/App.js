fc72240dec9c22395d7fa45069d6f29341cf6d54
Vedant Chakkarwar <vedantchakkarwar@gmail.com>
2024-09-25 14:17:17 +0530

    Code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, welcome to my React app hosted on AWS Amplify!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'Exp_6\my-react-app\src\index.css';
+import App from 'Exp_6\my-react-app\src\App.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
